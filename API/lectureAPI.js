c58d80a8578ee8a3c597c85bb068335d022d3d2b
minhalzahid <minhalbutt403@gmail.com>
2020-05-21 23:44:30 +0500

    changes

--- API/lectureAPI.js
+++ API/lectureAPI.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { URL } from './apiURLs';
 
 
-export const createLecture = (course, topic, description, schedule, link) => {
-    const body = { course, topic, description, schedule, link }
+export const createLecture = (course, topic, description, schedule, link, teacher_id) => {
+    const body = { course, topic, description, schedule, link , teacher_id}
 
     return axios.post(URL.createLecture, body)
 }
